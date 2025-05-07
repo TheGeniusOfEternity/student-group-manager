4a8d72d285cf911cc16112bc2e6a9ba30ae88b91
TheGeniusOfEternity <max06safin@yandex.ru>
2025-05-07 22:07:01 +0300

    fix(common): remove unnecessary package

--- common/src/main/kotlin/annotations/Nested.kt
+++
@@ -1,8 +0,0 @@
-package annotations
-import collection.StudyGroup
-/**
- * Annotation for nested properties like [StudyGroup.coordinates] & [StudyGroup.groupAdmin]
- */
-@Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.PROPERTY)
-annotation class Nested()
--- common/src/main/kotlin/exceptions/EmptyValueException.kt
+++
@@ -1,8 +0,0 @@
-package exceptions
-
-/**
- * Exception of empty value
- *
- * @param message Message about cause of exception
- */
-class EmptyValueException(message: String) : Exception(message)
